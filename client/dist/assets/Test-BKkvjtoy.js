8781f2617ab5e85b2efa754f26bcfaefb5b7333d
Suphal Bochkar <suphalbochkar@gmail.com>
2024-04-16 21:00:42 +0530

    dist remove

--- client/dist/assets/Test-BKkvjtoy.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-UE1Tgrxy.js";import"./atoms-Df3oWJGX.js";import"./selectors-DYpx1YuR.js";import{S as s}from"./SubscriptionInfo-Cj58-605.js";const e=()=>r.jsx("div",{className:"bg-[#f3f4f6]",children:r.jsx(s,{})});export{e as default};
