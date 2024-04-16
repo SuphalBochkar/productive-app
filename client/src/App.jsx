1f7b9e613c8307bd2fef7582f2e671728357d8d9
Suphal Bochkar <suphalbochkar@gmail.com>
2024-04-16 20:55:51 +0530

    app.jsx update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,22 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "../context/AuthContext";
 
+import Signin from "../pages/Signin";
+import Signup from "../pages/Signup";
+import MySpace from "../pages/MySpace";
+import UserChat from "../pages/UserChat";
+import Calendar from "../pages/Calendar";
+import Trash from "../pages/Trash";
+
 // Lazy-loaded components
-const Signup = React.lazy(() => import("../pages/Signup"));
-const Signin = React.lazy(() => import("../pages/Signin"));
-const MySpace = React.lazy(() => import("../pages/MySpace"));
-const Test = React.lazy(() => import("../pages/Test"));
-const TempSignout = React.lazy(() => import("../pages/TempSignout"));
-const UserChat = React.lazy(() => import("../pages/UserChat"));
-const Calendar = React.lazy(() => import("../pages/Calendar"));
-const Trash = React.lazy(() => import("../pages/Trash"));
+// const Signup = React.lazy(() => import("../pages/Signup"));
+// const Signin = React.lazy(() => import("../pages/Signin"));
+// const MySpace = React.lazy(() => import("../pages/MySpace"));
+// const Test = React.lazy(() => import("../pages/Test"));
+// const TempSignout = React.lazy(() => import("../pages/TempSignout"));
+// const UserChat = React.lazy(() => import("../pages/UserChat"));
+// const Calendar = React.lazy(() => import("../pages/Calendar"));
+// const Trash = React.lazy(() => import("../pages/Trash"));
 
 function App() {
   const { authUser } = useAuthContext();
@@ -21,7 +28,7 @@ function App() {
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route
-          path="/myspace"
+          path="/"
           element={
             authUser ? <Navigate to="/myspace" /> : <Navigate to="/signin" />
           }
