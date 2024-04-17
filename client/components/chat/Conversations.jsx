d6d8a72b03838fc713ad98a6d9d50049c0f03aad
Suphal Bochkar <suphalbochkar@gmail.com>
2024-04-17 15:04:35 +0530

    Update in skeleton

--- client/components/chat/Conversations.jsx
+++ client/components/chat/Conversations.jsx
@@ -42,45 +42,97 @@ const Conversations = () => {
   const { onlineUsers } = useSocketContext();
   const isOnline = (userId) => onlineUsers.includes(userId);
 
+  console.log("Search users", users);
   return (
     <div className="">
-      {conversationLoading && <MessageSkeleton n={10} />}
-      {usersToDisplay.map((user) => (
-        <div
-          key={user.user._id}
-          className="flex hover:bg-[#6161ff] hover:text-white hover:cursor-pointer m-1 py-3 px-5 rounded-lg items-center border border-gray-300"
-          onClick={() => handleUserSelect(user.user)}
-        >
-          <div className="hidden">ID: {user.user._id}</div>
-          <div className="w-max">
-            <div className="relative w-16 h-16 rounded-full mr-3 bg-gray-300 flex justify-center items-center">
-              <img
-                className="w-14 h-14"
-                src={user.user.profilePic}
-                alt="User Img"
-              />
-              <span
-                className={`bottom-0 left-12 absolute w-4 h-4 ${
-                  isOnline(user.user._id) ? `bg-green-400` : `bg-red-400`
-                } border-2 border-white dark:border-gray-800 rounded-full`}
-              ></span>
-            </div>
-          </div>
-          <div className="w-full">
-            <div className="flex justify-between items-center">
-              <div className="text-xl font-bold">
-                {toTitleCase(user.user.username)}
+      {/* {usersLoading && <MessageSkeleton n={4} />} */}
+      {search ? (
+        usersLoading ? (
+          <MessageSkeleton n={8} />
+        ) : users.length === 0 ? (
+          <div>No user found</div>
+        ) : (
+          users.map((user) => (
+            <div key={user._id} className="group">
+              <div
+                className="flex hover:bg-[#6161ff] hover:text-white hover:cursor-pointer m-1 py-3 px-5 rounded-lg items-center border border-gray-300 group"
+                onClick={() => handleUserSelect(user)}
+              >
+                <div className="w-max">
+                  <div className="relative w-16 h-16 rounded-full mr-3 bg-gray-300 flex justify-center items-center">
+                    <img
+                      className="w-14 h-14"
+                      src={user.profilePic}
+                      alt="User Img"
+                    />
+                  </div>
+                </div>
+                <div className="w-full">
+                  <div className="flex justify-between items-center">
+                    <div className="text-xl font-bold">
+                      {toTitleCase(user.username)}
+                    </div>
+                    <div className="px-3 py-2 rounded-md bg-[#6161ff] text-white hover:bg-white hover:text-[#6161ff]">
+                      Send Message
+                    </div>
+                  </div>
+                </div>
               </div>
-              {user.lastMessageTime && (
-                <div>{extractTime(user.lastMessageTime)}</div>
-              )}
             </div>
-            {user.lastMessage && <div>{user.lastMessage}</div>}
-          </div>
+          ))
+        )
+      ) : (
+        <div>
+          {conversationLoading ? (
+            <MessageSkeleton n={6} />
+          ) : conversations.length === 0 ? (
+            <div>No Chat</div>
+          ) : (
+            conversations.map((user) => (
+              <div
+                key={user.user._id}
+                className="flex hover:bg-[#6161ff] hover:text-white hover:cursor-pointer m-1 py-3 px-5 rounded-lg items-center border border-gray-300"
+                onClick={() => handleUserSelect(user.user)}
+              >
+                <div className="w-max">
+                  <div className="relative w-16 h-16 rounded-full mr-3 bg-gray-300 flex justify-center items-center">
+                    <img
+                      className="w-14 h-14"
+                      src={user.user.profilePic}
+                      alt="User Img"
+                    />
+                    <span
+                      className={`bottom-0 left-12 absolute w-4 h-4 ${
+                        isOnline(user.user._id) ? `bg-green-400` : `bg-red-400`
+                      } border-2 border-white dark:border-gray-800 rounded-full`}
+                    ></span>
+                  </div>
+                </div>
+                <div className="w-full">
+                  <div className="flex justify-between items-center">
+                    <div className="text-xl font-bold">
+                      {toTitleCase(user.user.username)}
+                    </div>
+                    {user.lastMessageTime && (
+                      <div>{extractTime(user.lastMessageTime)}</div>
+                    )}
+                  </div>
+                  {user.lastMessage && <div>{user.lastMessage}</div>}
+                </div>
+              </div>
+            ))
+          )}
         </div>
-      ))}
+      )}
     </div>
   );
 };
 
 export default Conversations;
+
+// {
+//   "_id": "6617e1e05f7fc797971c6ee9",
+//   "username": "sai1",
+//   "email": "sai1@gmail.com",
+//   "profilePic": "https://api.dicebear.com/8.x/lorelei/svg?seed=sai1"
+// }
--- client/components/chat/MessageSkeleton.jsx
+++ client/components/chat/MessageSkeleton.jsx
@@ -2,18 +2,25 @@ import React from "react";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 const MessageSkeleton = ({ n }) => {
-  return Array(n)
-    .fill(0)
-    .map((_, i) => (
-      <SkeletonTheme baseColor="#cacccf" highlightColor="#e6e3e3">
-        <div className="flex m-1 py-3 px-5 rounded-lg items-center">
-          <Skeleton circle width={40} height={40} className="mr-3" />
-          <div className="text-lg font-bold">
-            <Skeleton width={270} height={20} />
-          </div>
-        </div>
-      </SkeletonTheme>
-    ));
+  return (
+    <>
+      {Array(n)
+        .fill(0)
+        .map((_, i) => (
+          <SkeletonTheme key={i} baseColor="#cacccf" highlightColor="#e6e3e3">
+            <div className="flex m-1 py-3 px-5 rounded-lg items-center">
+              <Skeleton circle width={60} height={60} className="mr-3" />
+              <div className="flex items-center w-max">
+                <div className="text-lg font-bold">
+                  <Skeleton width={200} height={20} />
+                  <Skeleton width={150} height={20} />
+                </div>
+              </div>
+            </div>
+          </SkeletonTheme>
+        ))}
+    </>
+  );
 };
 
 export default MessageSkeleton;
